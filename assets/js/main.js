2f1623287fac446bf8d056125559c21e68937290
iamjosh <58791428+iamjoshade@users.noreply.github.com>
2024-07-06 13:03:35 +0100

    added footer area

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,13 +7,15 @@ const navBgOverlayEl = document.querySelector('.nav__bgOverlay');
 const navOpen = () => {
     navList.classList.add('show');
     navBgOverlayEl.classList.add('active');
-    document.body.style = 'visibility:visible, height:100vh, width:100vw, overflow:hidden;';
+    document.body.style = 'visibility:visible; height:100vh; width:100vw; overflow:hidden;';
 }
 
 const navClose = () => {
     navList.classList.remove('show');
     navBgOverlayEl.classList.remove('active');
-    document.body.style = 'visibility:visible, height:initial, width:100%, overflow-x:hidden;'; 
+    document.body.style = 'visibility:visible; height:initial; width:100%; overflow-x:hidden;'; 
 }
 
 navIconEl.addEventListener('click', navOpen);
+ navCloseEl.addEventListener('click', navClose);
+ navBgOverlayEl.addEventListener('click', navClose);
